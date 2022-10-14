46ff3d965b1b31d9a71ad1070f4f57b59375556f
stevenaeola <s.p.bradleydurham.ac.uk>
2022-10-14 10:50:14 +0100

    Move reveal slides to mathjax

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mustache ../templates/reveal-config.json ../templates/mathjax-macros.md > README.md
+#mustache ../templates/reveal-config.json ../templates/mathjax-macros.md > README.md
 
-sed '/^---/ d' PITCHME.md | sed '/^@/ d' | sed '/^:::/ d' | sed 's/{.*}//' >> README.md
+sed '/^---/ d' PITCHME.md  > README.md
 
 PATHEND=$(pwd | rev | cut -d'/' -f-2 | rev)
 
 # install mustache with npm i -g mustache
 
 mustache ../templates/reveal-config.json ../templates/reveal_top.mustache > PITCHME.html
-mustache ../templates/reveal-config.json ../templates/katex-macros.md >> PITCHME.html
+#mustache ../templates/reveal-config.json ../templates/mathjax-macros.md >> PITCHME.html
 mustache ../templates/reveal-config.json PITCHME.md >> PITCHME.html
 mustache ../templates/reveal-config.json ../templates/reveal_bottom.mustache >> PITCHME.html
 
--- templates/mathjax-macros.md
+++ templates/mathjax-macros.md
@@ -1,4 +1,1 @@
-$$\def\V#1{\mathbf{#1}}$$
-$$\newcommand{\R}{\mathbb{R}}$$
-$$\def\Vthree#1#2#3{\begin{pmatrix} #1 \\ #2 \\  #3 \end{pmatrix} }$$
-$$\def\Vthrees#1#2#3{\Big(\begin{smallmatrix} #1 \\ #2 \\  #3 \end{smallmatrix} \Big)}$$
+`$$\def\R{\Bbb{R}}$$`
